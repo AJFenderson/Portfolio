4f462e2d471ed6e9fb502a56ef19a0c0ac468f93
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-07-25 22:36:43 -0500

    added buttons for app display, project.js, project array

---
+++ Assets/scripts/projects.js
@@ -0,0 +1,16 @@
+var projectsArr = {
+    "route404": {
+        title: "Route 404 - Travel bucket list App",
+        type: "heroku",
+        desc: `This application will allow a user to create a travel log using their destination of choice.`,
+        pageUrl: "https://route-404.herokuapp.com/",
+        hubUrl: "https://github.com/AJFenderson/aRoute-404"
+    },
+    "groupFinder": {
+        title: "Group Finder",
+        type: "react",
+        desc: "Friend Finder is an application that uses the users unique answers from our personality quiz to pair them with a special character from our database.",
+        pageUrl: "https://friendfinderappthatiscool.herokuapp.com/",
+        hubUrl: "https://github.com/AJFenderson/groupFinder"
+    }
+}
